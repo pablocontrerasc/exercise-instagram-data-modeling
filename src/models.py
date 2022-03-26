04fe215618d5535615997baa76c8ff53e606000b
pablocontrerasc <cont.pab@gmail.com>
2022-03-26 22:15:13 +0000

    Modelo instagram ok

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,26 +8,55 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    lastName = Column(String(100), nullable= False)
+    username = Column(String(100), nullable= False)
+    email = Column(String(100),nullable= False)
+    phone = Column(String(100),nullable= False)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    photo = Column(String(250))
+    description = Column(String, nullable = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment = Column(String(250))
+    create_at = Column(String, nullable = True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
+
+association_table = Table('Like', Base.metadata,
+                          Column('user_id', ForeignKey('user.id')),
+                          Column('post_id', ForeignKey('post.id'))
+                          )
+class Follower(Base):
+    __tablename__ = 'follower'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    confirm = Column(String, nullable= False)
+    create_at = Column(String, nullable = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 ## Draw from SQLAlchemy base
 try:
